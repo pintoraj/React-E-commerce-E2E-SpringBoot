3f074ee2c558b95c013c95c5df49eb784aaf4b02
Pint08 <pinto28130@gmail.com>
2025-10-26 21:51:39 +0530

    Debugged Security Features

--- ReactEcommerceBackend/src/main/java/com/pinto/ReactEcommerceBackend/config/WebConfig.java
+++ ReactEcommerceBackend/src/main/java/com/pinto/ReactEcommerceBackend/config/WebConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
-	
-	public void addCorsMapping(CorsRegistry registry) {
-			registry.addMapping("/**")
-			.allowedOrigins("http://localhost:5173")
-			.allowedMethods("GET", "POST" , "PUT" , "DELETE" , "PATCH", "OPTIONS")
-			.allowedHeaders("*")
-			.allowCredentials(true);
-	}
-	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
+
